e36bc48a105bc86d2125b136da51f4c0b0b9be5e
igorbc <igorbuenocorrea@gmail.com>
2016-05-13 17:54:36 -0300

    4 array (true)
    An array with all the values for all possible groups of four is being
    populated now. The program can evaluete if the match is over and the
    user can have lots of feedback.

--- four.c
+++ four.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 
+#define P1_WINS 1
+#define P2_WINS -1
+#define DRAW 0
+#define ACTIVE_GAME 2
+
 const int rows = 6;
 const int columns = 7;
 
 int timebank, time_per_move, your_botid, game_round, time_left;
 char line[1000], player_names[1000], your_bot[1000], field[1000];
+
+int four_array[69][5];
 
 typedef enum {min_player = -1, blank = 0, max_player = 1} t_player;
 
@@ -13,39 +20,264 @@ typedef t_player t_state[6][7];
 
 t_state state;
 
-void print()
+char value_to_char(int value) 
 {
-	for (int i = rows-1; i >= 0; i--)
-	{
-		for (int j = 0; j < columns; j++)
-		{
-			char c = state[i][j] == 1 ? '*' : '#';
-			c = state[i][j] == 0 ? '.' : c;
-			printf("%c ", c);
-		}
-		printf("\n");
-	}
+    return (value == 1) ? 2 : (value == 0) ? 250 : 1;
+}
+
+void print_gamefield()
+{
+    int i, j;
+    printf("game:\n");
+    
+    for (i = rows-1; i >= 0; i--)
+    {
+        for (j = 0; j < columns; j++)
+        {
+            printf(" %c", value_to_char(state[i][j]));
+        }
+        printf("\n");
+    }
+}
+
+void print_is_terminal(){
+
+    switch(is_terminal(state))
+    {
+    case DRAW: 
+        printf("game ended in a draw\n");
+        break;
+
+    case ACTIVE_GAME:
+        printf("game still going on\n");
+        break;
+
+    case P1_WINS:
+        printf("player 1 wins!\n");
+        break;
+
+    case P2_WINS:
+        printf("player 2 wins!\n");
+    }
+}
+
+void update_four_array(t_state s)
+{
+    int fa_count = 0, i, j;
+    int sum;
+    
+    for(i = 0; i < rows; i++) /* groups of 4 in rows*/
+    {
+        for(j = 0; j < 4; j++)
+        {
+  
+            sum = 0;
+            sum += four_array[fa_count][0] = s[i][ j ];
+            sum += four_array[fa_count][1] = s[i][j+1];
+            sum += four_array[fa_count][2] = s[i][j+2];
+            sum += four_array[fa_count][3] = s[i][j+3];
+            four_array[fa_count][4] = sum;
+            fa_count++;
+        }
+
+    }
+    for(i = 0; i < columns; i++) /* groups of 4 in columns*/
+    {
+        for(j = 0; j < 3; j++)
+        {
+            sum = 0;
+            sum += four_array[fa_count][0] = s[ j ][i];
+            sum += four_array[fa_count][1] = s[j+1][i];
+            sum += four_array[fa_count][2] = s[j+2][i];
+            sum += four_array[fa_count][3] = s[j+3][i];
+            four_array[fa_count][4] = sum;
+    
+            fa_count++;
+        }
+    }
+    
+    int diagonal_groups_to_get[] = {1,2,3,3,2,1};
+    int diagonal_starting_points_positive[][2] = {0,3, 0,2, 0,1, 0,0, 1,0, 2,0};
+    int diagonal_starting_points_negative[][2] = {3,0, 4,0, 5,0, 5,1, 5,2, 5,3};
+    int diagonal_starting_points_counter;
+    int diagonal_4_counter;
+    
+    int k;
+
+    for(diagonal_starting_points_counter = 0, diagonal_4_counter = 0;
+        diagonal_starting_points_counter < 6; 
+        diagonal_starting_points_counter++,
+        diagonal_4_counter++){
+
+        i = diagonal_starting_points_positive[diagonal_starting_points_counter][0];
+        j = diagonal_starting_points_positive[diagonal_starting_points_counter][1];
+        for(k = 0; k < diagonal_groups_to_get[diagonal_4_counter]; k++, i++, j++){
+           sum = 0;
+           sum += four_array[fa_count][0] = s[i][j];
+           sum += four_array[fa_count][1] = s[i+1][j+1];
+           sum += four_array[fa_count][2] = s[i+2][j+2];
+           sum += four_array[fa_count][3] = s[i+3][j+3];
+           four_array[fa_count][4] = sum;
+    
+           fa_count++;
+       }
+    }
+
+    for(diagonal_starting_points_counter = 0, diagonal_4_counter = 0;
+        diagonal_starting_points_counter < 6; 
+        diagonal_starting_points_counter++,
+        diagonal_4_counter++){
+
+        i = diagonal_starting_points_negative[diagonal_starting_points_counter][0];
+        j = diagonal_starting_points_negative[diagonal_starting_points_counter][1];
+        for(k = 0; k < diagonal_groups_to_get[diagonal_4_counter]; k++, i--, j++){
+           sum = 0;
+           sum += four_array[fa_count][0] = s[i][j];
+           sum += four_array[fa_count][1] = s[i-1][j+1];
+           sum += four_array[fa_count][2] = s[i-2][j+2];
+           sum += four_array[fa_count][3] = s[i-3][j+3];
+           four_array[fa_count][4] = sum;
+    
+           fa_count++;
+       }
+    }
+}
+
+int print_four_array()
+{
+    int i;
+
+    update_four_array(state);
+
+    printf("\nhorizontal\n");
+    for(i = 0; i < 24; i++)
+    {
+        printf("%c %c %c %c sum: %d\n", value_to_char(four_array[i][0]),
+                                   value_to_char(four_array[i][1]),
+                                   value_to_char(four_array[i][2]),
+                                   value_to_char(four_array[i][3]),
+                                   four_array[i][4]);
+    }
+    printf("\nvertical\n");
+    for(i = 24; i < 24+21; i++)
+    {
+        printf("%c %c %c %c sum: %d\n", value_to_char(four_array[i][0]),
+                                   value_to_char(four_array[i][1]),
+                                   value_to_char(four_array[i][2]),
+                                   value_to_char(four_array[i][3]),
+                                   four_array[i][4]);
+    }
+    
+    printf("\npositive diagonal\n");
+    for(i = 24+21; i < 24+21+12; i++)
+    {
+        printf("%c %c %c %c sum: %d\n", value_to_char(four_array[i][0]),
+                                   value_to_char(four_array[i][1]),
+                                   value_to_char(four_array[i][2]),
+                                   value_to_char(four_array[i][3]),
+                                   four_array[i][4]);
+    }
+    
+    printf("\nnegative diagonal\n");
+    for(i = 24+21+12; i < 24+21+12+12; i++)
+    {
+        printf("%c %c %c %c sum: %d\n", value_to_char(four_array[i][0]),
+                                   value_to_char(four_array[i][1]),
+                                   value_to_char(four_array[i][2]),
+                                   value_to_char(four_array[i][3]),
+                                   four_array[i][4]);
+    }
+        
+}
+
+int is_terminal(t_state s)
+{
+    int i;
+    update_four_array(s);
+
+    for (i = 0; i < 69; i++)
+    {
+        if(four_array[i][4] == -4)
+            return P2_WINS;
+        else if (four_array[i][4] == -4)
+            return P1_WINS;
+    }
+
+    for(i = 0; i < columns; i++)
+        if(s[5][i]==0) return ACTIVE_GAME; 
+        
+    return DRAW;
+}
+
+int utility(t_state s){
+    return 0;
+}
+
+int min_value(t_state s){
+    return 0;
+}
+
+int max_value(t_state s)
+{
+    t_state successor;
+
+    int v = -15000;
+    int current_min;
+
+    if(is_terminal(s))
+        return	utility(s);
+
+    for	(;;){ /* each successor of state*/	
+        current_min = min_value(successor);
+        v = (v > current_min)? v : current_min;
+    }
+    return	v;
 }
 
 void update_gamefield(char field[])
 {
-	int pos = 0, inc;
+    int pos = 0, inc, i, j ;
 
-	for (int i = rows-1; i >= 0; i--)
-		for (int j = 0; j < columns; j++)
-		{
-			sscanf(&field[pos], "%d%*[,;]%n", &state[i][j], &inc);
-			pos += inc;
-			if (state[i][j] == your_botid)
-				state[i][j] = max_player;
-			else if (state[i][j] != 0)
-				state[i][j] = min_player;
-		}
+    for (i = rows-1; i >= 0; i--)
+        for (j = 0; j < columns; j++)
+        {
+            sscanf(&field[pos], "%d%*[,;]%n", &state[i][j], &inc);
+            pos += inc;
+            if (state[i][j] == your_botid)
+                state[i][j] = max_player;
+            else if (state[i][j] != 0)
+                state[i][j] = min_player;
+        }
+}
+
+void print_settings()
+{
+    printf("settings timebank %d\n", timebank);
+    printf("settings time_per_move %d\n", time_per_move);
+    printf("settings player_names %s\n", player_names);
+    printf("settings your_bot %s\n", your_bot);
+    printf("settings your_botid %d\n", your_botid);
 }
 
 void play()
 {
-	printf("place_disc %d\n", 0);
+	printf("place_disc %d\n", rand()%7);
+}
+
+void print_ascii()
+{
+    int i;
+    for(i = 0; i < 256; printf("%c %d\n", i++, i));
+}
+
+void initialize_settings()
+{
+    timebank = 10000;
+    time_per_move = 500;
+    strcpy(player_names,"player1,player2");
+    strcpy(your_bot,"player1");
+    your_botid = 1;
+    srand(time(NULL));
 }
 
 int main()
@@ -54,20 +286,32 @@ int main()
 	t_player player;
 
 	setbuf(stdout, NULL);
-	while (1)
+
+    initialize_settings();
+
+	while(1)
 	{
-		if (gets(line) == NULL) break;
+        if (gets(line) == NULL) break;
 		if (strlen(line) == 0) continue;
+
+        /*printf("line: %s\n", line);*/
+
 		sscanf(line, "settings timebank %d", &timebank);
 		sscanf(line, "settings time_per_move %d", &time_per_move);
 		sscanf(line, "settings player_names %s", player_names);
 		sscanf(line, "settings your_bot %s", your_bot);
+        
 		sscanf(line, "settings your_botid %d", &your_botid);
-		// sscanf(line, "settings field_columns %d", &field_columns); 	// unsupported
-		// sscanf(line, "settings field_rows %d", &field_rows); 		// unsupported
+		/* sscanf(line, "settings field_columns %d", &field_columns); 	*/
+		/* sscanf(line, "settings field_rows %d", &field_rows); 		*/ 
 		sscanf(line, "update game round %d", &game_round);
 		if (sscanf(line, "update game field %s", field)) update_gamefield(field);
 		if (sscanf(line, "action move %d", &time_left)) play();
+        if (strcmp(line, "field")==0) print_gamefield();
+        if (strcmp(line, "dump")==0) print_settings();
+        if (strcmp(line, "ascii")==0) print_ascii();
+        if (strcmp(line, "terminal")==0) print_is_terminal();
+        if (strcmp(line, "print4")==0) print_four_array();
 	}
 	return 0;
 }
